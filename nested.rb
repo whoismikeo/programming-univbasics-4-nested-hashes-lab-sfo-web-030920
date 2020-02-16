94fdc232be5d476a4cde58290e924f10efd941e9
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-16 23:02:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,12 +82,13 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         },
+        :yukihiro_matsumoto => {
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+}
         
      }
 
-programmer_hash[:yukihiro_matsumoto] = {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
 }
     
 end
